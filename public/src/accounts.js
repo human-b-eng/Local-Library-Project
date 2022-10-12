271d14b5bea2ed3d6ce638e2b1ffc99afedd7221
human-b-eng <brian.eng6@gmail.com>
2022-10-11 19:51:21 -0600

    10.11.22 updates

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,31 +1,38 @@
 //d1//returns the account object that has the matching ID.
-function findAccountById(accounts=[], id) {
-  return accounts.find((account)=>account.id === id)
+function findAccountById(accounts = [], id) {
+  return accounts.find((account) => account.id === id);
 }
 
 //d1//returns a sorted array of the provided account objects. The objects are sorted alphabetically by last name.
-function sortAccountsByLastName(accounts=[]) {
-  return accounts.sort((acctA,acctB)=>acctA.name.last.toLowerCase() > acctB.name.last.toLowerCase()? 1 : -1)
+function sortAccountsByLastName(accounts = []) {
+  return accounts.sort((acctA, acctB) =>
+    acctA.name.last.toLowerCase() > acctB.name.last.toLowerCase() ? 1 : -1
+  );
 }
 
 //d2// return a number that represents the number of times the account's ID appears in any book's borrows array.
 function getTotalNumberOfBorrows(account, books) {
-let borrowed = books.filter((book)=>book.borrows.some((borrow)=>borrow.id === account.id))
-return borrowed.length
+  const borrowed = books.filter((book) =>
+    book.borrows.some((borrow) => borrow.id === account.id)
+  );
+  return borrowed.length;
+  ////TRY REDOING WITH REDUCE////
 }
-
 
 //d2// returns an array of book objects, including author information, that represents all books currently checked out by the given account. Look carefully at the object below, as it's not just the book object; the author object is nested inside of it.
 function getBooksPossessedByAccount(account, books, authors) {
-  let borrowedBooks = books.filter((book)=>book.borrows.some((borrow)=>borrow.id === account.id && borrow.returned === false))
-  let result = borrowedBooks.map((book)=>{
-    let foundAuthor = authors.find((author)=>author.id === book.authorId)
-    book.author = foundAuthor
-    return book
-  })
-  return result
+  const borrowedBooks = books.filter((book) =>
+    book.borrows.some(
+      (borrow) => borrow.id === account.id && borrow.returned === false
+    )
+  );
+  const result = borrowedBooks.map((book) => {
+    const foundAuthor = authors.find((author) => author.id === book.authorId);
+    book.author = foundAuthor;
+    return book;
+  });
+  return result;
 }
-
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,36 +1,36 @@
 //d1//returns the author object that has the matching ID.
-function findAuthorById(authors=[], id="") {
-  return authors.find((author)=>author.id === id)
+function findAuthorById(authors = [], id = "") {
+  return authors.find((author) => author.id === id);
 }
-
 
 //d1//returns the book object that has the matching ID.
-function findBookById(books=[], id="") {
-  return books.find((book)=>book.id === id)
+function findBookById(books = [], id = "") {
+  return books.find((book) => book.id === id);
 }
-
 
 //d2//returns an array with two arrays inside of it. All of the inputted books are present in either the first or second array.
 function partitionBooksByBorrowedStatus(books) {
-  let borrowed = books.filter((book)=>book.borrows.some((borrow)=>borrow.returned === false))
-  let returned = books.filter((book)=>book.borrows.every((borrow)=>borrow.returned === true))
-  return [borrowed,returned]
+  const borrowed = books.filter((book) =>
+    book.borrows.some((borrow) => borrow.returned === false)
+  );
+  const returned = books.filter((book) =>
+    book.borrows.every((borrow) => borrow.returned === true)
+  );
+  return [borrowed, returned];
 
-  //return [books.filter((book)=>book.borrows.some((borrow)=>borrow.returned === false)),books.filter((book)=>book.borrows.every((borrow)=>borrow.returned === true))]
 }
 
-
 //d2//should return an array of ten or fewer account objects that represents the accounts given by the IDs in the provided book's borrows array. However, each account object should include the returned entry from the corresponding transaction object in the borrows array.
-function getBorrowersForBook(book={}, accounts=[]) {
-let {borrows} = book
-let result = borrows.map((borrower)=>{
-  let foundBorrower = accounts.find((account)=>{
-    return account.id === borrower.id
-  })
-  foundBorrower.returned = borrower.returned
-  return foundBorrower
-  })
-return result.slice(0, 10)
+function getBorrowersForBook(book = {}, accounts = []) {
+  const { borrows } = book;
+  const result = borrows.map((borrower) => {
+    const foundBorrower = accounts.find((account) => {
+      return account.id === borrower.id;
+    });
+    foundBorrower.returned = borrower.returned;
+    return foundBorrower;
+  });
+  return result.slice(0, 10);
 }
 
 module.exports = {
